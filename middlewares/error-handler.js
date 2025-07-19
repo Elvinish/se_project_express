a4e2be3aa9294640e33a1b54106bededad2ea9a5
Elvin Vandamov <Elvin@MacBook-Air.local>
2025-07-19 02:08:21 -0700

    fixed

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,15 +1,11 @@
-const { STATUS_CODES } = require("../utils/constants");
-
 function errorHandler(err, req, res, next) {
   console.error(err.stack || err.message);
   console.log("DEBUG ERROR OBJECT:", err);
 
-  const statusCode = err.statusCode || STATUS_CODES.INTERNAL_SERVER_ERROR;
+  const statusCode = err.statusCode || 500;
 
   const message =
-    statusCode === STATUS_CODES.INTERNAL_SERVER_ERROR
-      ? "An internal server error occurred"
-      : err.message;
+    statusCode === 500 ? "An internal server error occurred" : err.message;
 
   res.status(statusCode).send({ message });
 }
