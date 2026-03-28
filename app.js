5a977a63e95dfbc7fa2c503316f69912110337cd
Elvin Vandamov <Elvin@MacBook-Air.local>
2026-03-28 03:17:15 -0700

    prepare backend for vercel

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
-const { PORT = 3001 } = process.env;
+// const { PORT = 3001 } = process.env;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to DB");
   })
@@ -35,6 +35,4 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+module.exports = app;
